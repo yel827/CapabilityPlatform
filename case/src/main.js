4cb294b152b73fe6ac9fe5a0207a7b5d75261802
yel <y15806701516@163.com>
2019-10-12 08:59:17 +0800

    租户管理

--- case/src/main.js
+++ case/src/main.js
@@ -3,7 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 
+import axios from 'axios'
+
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- case/src/router/index.js
+++ case/src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import home from '@/components/home'
+import Administration from "@/components/Administration"
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: home
+    },
+    {
+      path: '/Administration',
+      name: 'Administration',
+      component: Administration
     }
   ]
 })
